6802ba068951ae09085c20540799af9321594d64
Kevin Chou <kevchou@gmail.com>
2018-08-14 22:02:00 -0400

    created new functions

--- news.py
+++ news.py
@@ -5,14 +5,6 @@ import datetime as dt
 
 
 
-ticker = 'AAPL'
-
-# Generate list of dates
-start = '2018-01-01'
-end = '2018-01-07'
-
-
-
 def generate_date_list(start, end):
     start_dt = dt.datetime.strptime(start, '%Y-%m-%d')
     end_dt = dt.datetime.strptime(end, '%Y-%m-%d')
@@ -20,18 +12,7 @@ def generate_date_list(start, end):
     dates = [start_dt + dt.timedelta(days=x) for x in range(0, (end_dt - start_dt).days)]
     return dates
 
-dates = generate_date_list(start, end)
-
-
-
-# Convert date to right format for url
-dates = [f"{str(d.month).zfill(2)}{str(d.day).zfill(2)}{d.year}" for d in dates]
-
-# Stores all news articles
-all_news = []
-
-for i, date in enumerate(dates):
-    
+def get_story(ticker, date):
     url = f'https://www.reuters.com/finance/stocks/company-news/{ticker}.O?date={date}'
 
     # Get html from URL and create soup object
@@ -75,12 +56,26 @@ for i, date in enumerate(dates):
                 'url': story_link
             })
 
-    all_news.append(company_news)
-    if i % 25 == 0:
-        print(i)
+    return company_news
 
 
-print(len(all_news))
+
+ticker = 'AAPL'
+
+# Generate list of dates
+start = '2018-01-01'
+end = '2018-01-07'
+
+dates = generate_date_list(start, end)
+
+# Convert date to right format for url
+dates = [f"{str(d.month).zfill(2)}{str(d.day).zfill(2)}{d.year}" for d in dates]
+
+# Stores all news articles
+all_news = []
+
+for i, date in enumerate(dates):
+    all_news.append(get_story(ticker, date))
 
 
 ##### Get full news article
